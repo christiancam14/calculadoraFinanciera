281eec37aa3777f20db88ab98929d6459afa9517
Christian Stiven Camacho Galvis <chriscamacho1045@gmail.com>
2024-09-21 14:40:44 -0500

    Permisos

--- src/simulatorApp.tsx
+++ src/simulatorApp.tsx
@@ -1,13 +1,48 @@
 import 'react-native-gesture-handler';
 
-import {useColorScheme} from 'react-native';
+import {PermissionsAndroid, Platform, useColorScheme} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import * as eva from '@eva-design/eva';
 import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
 import {DrawerNagivator} from './presentation/routes/DrawerNagivator';
+import {useEffect} from 'react';
+
+async function requestNotificationPermission() {
+  if (Platform.OS === 'android' && Platform.Version >= 33) {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+        {
+          title: 'Permiso de notificaciones',
+          message:
+            'Esta aplicación necesita permiso para enviarte notificaciones',
+          buttonPositive: 'Aceptar',
+        },
+      );
+
+      return granted === PermissionsAndroid.RESULTS.GRANTED;
+    } catch (err) {
+      console.warn('Error solicitando permisos de notificación', err);
+      return false;
+    }
+  }
+  return true;
+}
 
 export const simulatorApp = () => {
+  useEffect(() => {
+    // Llama a la función para solicitar permisos
+    requestNotificationPermission().then(granted => {
+      if (granted) {
+        console.log('Permiso de notificaciones concedido');
+        // Aquí puedes agregar cualquier configuración adicional de notificaciones
+      } else {
+        console.log('Permiso de notificaciones denegado');
+      }
+    });
+  }, []);
+
   const colorScheme = useColorScheme();
   const theme = colorScheme === 'dark' ? eva.dark : eva.light;
   const backgroundColor =
