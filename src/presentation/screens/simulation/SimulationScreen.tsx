ff911eb56200391594c09d8524a7c42435de4811
Christian Stiven Camacho Galvis <chriscamacho1045@gmail.com>
2024-09-21 16:26:04 -0500

    Correjidas notificaciones - estable

--- src/presentation/screens/simulation/SimulationScreen.tsx
+++ src/presentation/screens/simulation/SimulationScreen.tsx
@@ -1,9 +1,39 @@
-import {Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {Button} from '@ui-kitten/components';
+// import {TextInput} from 'react-native-gesture-handler';
+// import {scheduleNotification} from '../../../config/helpers/scheduleNotification';
+
+import {scheduleNotification} from '../../../config/helpers/scheduleNotification';
 
 export const SimulationScreen = () => {
+  const handleScheduleNotification = async () => {
+    try {
+      const scheduleDate = new Date();
+      scheduleDate.setSeconds(scheduleDate.getSeconds() + 5);
+
+      await scheduleNotification({
+        date: scheduleDate,
+        notificationTitle: 'Recordatorio guardado',
+        notificationBody: 'Este es el cuerpo de tu recordatorio',
+      });
+      console.log('Notificación programada exitosamente.');
+    } catch (error) {
+      console.error('Error programando la notificación:', error);
+      console.log('Error al programar la notificación.');
+    }
+  };
+
   return (
-    <View>
-      <Text>SimulationScreen</Text>
+    <View style={styles.container}>
+      <Button onPress={handleScheduleNotification}>
+        Programar Notificación
+      </Button>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+});
--- src/simulatorApp.tsx
+++ src/simulatorApp.tsx
@@ -1,46 +1,25 @@
 import 'react-native-gesture-handler';
 
-import {PermissionsAndroid, Platform, useColorScheme} from 'react-native';
+import {useColorScheme} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import * as eva from '@eva-design/eva';
 import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
 import {DrawerNagivator} from './presentation/routes/DrawerNagivator';
 import {useEffect} from 'react';
-
-async function requestNotificationPermission() {
-  if (Platform.OS === 'android' && Platform.Version >= 33) {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
-        {
-          title: 'Permiso de notificaciones',
-          message:
-            'Esta aplicación necesita permiso para enviarte notificaciones',
-          buttonPositive: 'Aceptar',
-        },
-      );
-
-      return granted === PermissionsAndroid.RESULTS.GRANTED;
-    } catch (err) {
-      console.warn('Error solicitando permisos de notificación', err);
-      return false;
-    }
-  }
-  return true;
-}
+import notifee from '@notifee/react-native';
 
 export const simulatorApp = () => {
   useEffect(() => {
-    // Llama a la función para solicitar permisos
-    requestNotificationPermission().then(granted => {
-      if (granted) {
-        console.log('Permiso de notificaciones concedido');
-        // Aquí puedes agregar cualquier configuración adicional de notificaciones
-      } else {
-        console.log('Permiso de notificaciones denegado');
+    // Solicitar permiso para enviar notificaciones
+    async function requestUserPermission() {
+      const authStatus = await notifee.requestPermission();
+      if (authStatus.authorizationStatus !== 1) {
+        console.log('Permission not granted');
       }
-    });
+    }
+
+    requestUserPermission();
   }, []);
 
   const colorScheme = useColorScheme();
